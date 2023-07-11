330f4ca3ed29048d4e963daec1de527a41fc1c99
Vidya Dhopate <vidya.dhopate@michelin.com>
2023-07-11 11:35:50 +0530

    syncing existing changes

--- script.js
+++ script.js
@@ -17,3 +17,46 @@ accordionButtons.forEach(button => {
   });
 });
 
+// Make timeline component interactive 
+
+
+  // define variables
+  const items = document.querySelectorAll(".timeline li");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom*(0.75) <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    items.forEach(item => {
+      if(isElementInViewport(item)) {
+        item.classList.add('in-view')
+      } else {
+        item.classList.remove('in-view')
+      }
+    })
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+
+// toggle header link
+const menuIcon = document.querySelector('.menu-icon')
+const header = document.querySelector('.header-link')
+
+function responsiveNav() {
+  header.classList.toggle('responsive');
+}
+
+menuIcon.addEventListener("click", responsiveNav)
